68fc370eba30963fc45dd4976bc1ae650d709f0c
Jake <jakebirkes@gmail.com>
2020-09-12 13:10:20 -0700

    all components in intro included

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NewJSFeatures from './NewJSFeatures';
+import Card from './Card';
+import CardClass from './CardClass';
+import RoundedImg from "./RoundedImg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NewJSFeatures />
+      <div>
+        <Card cardColor="red" width={200} />
+        <Card cardColor="yellow" height={150} />
+        <Card cardColor="green" width={250} height={175} />
+        <CardClass />
+      </div>
+      <div>
+        <RoundedImg src="https://picsum.photos/id/237/300/300" />
+      </div>
+    </>
   );
 }
 
