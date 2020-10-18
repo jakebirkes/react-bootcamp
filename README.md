34090deb432b12d578073cb4df8029ec39f6b69c
Jake <jakebirkes@gmail.com>
2020-10-17 19:10:41 -0700

    init react router

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # React Router
+
+`yarn add react-dom`
+
+`yarn add react-router-dom`
+
+For Single-Page Applications.
+
+- Conditionally render large parts of your page.
+- Declarative API
+- Hooks 🎉
+
+## Primary Components
+
+- Routers: `<BrowserRouter>` & `<HashRouter>`
+
+### BrowserRouter
+
+Ii is a context provider.
+
+```js
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
+```
+
+- Route Matchers `<Route>` & `<Switch>`
+- Navigation ("route changers"): `<Link>`, `<NavLink>`, & `<Redirect>`
+
+## Hooks
+
+- `useHistory`
+- `useLocation`
+- `useParams`
--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
+import { Link, Switch, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer">
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
-}
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+const App = () => (
+	<div className="App">
+		<header className="App-header">
+			<img src={logo} className="App-logo" alt="logo" />
+			<nav>
+				<Link to="/">Home</Link>
+				<Link to="/about">About</Link>
+				<Link to="/contact">Contact</Link>
+			</nav>
+
+			<Switch>
+				{/* Different ways of using Route components */}
+				<Route exact path="/" render={() => <Home />} />
+				<Route path="/about" component={About} />
+				<Route path="/contact">
+					<Contact email="wantulok.jake@gmail.com" />
+				</Route>
+			</Switch>
+
+			<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+				Learn React
+			</a>
+		</header>
+	</div>
+);
 
 export default App;
